9bfd728c12873a251892d94e25ae7bd76eb168d4
Vilius <viliusarminas@gmail.com>
2017-04-06 15:30:35 +0300

    Pridedu duombazes/api projekta ir sukurtos duombazes pavyzdzio jpg

---
+++ NRD-Sporto-klubas/DB_API/sport-workouts-web-api/sport-workouts-web-api/Classes/WorkoutDay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sport_workouts_web_api.Classes
+{
+    public class WorkoutDay
+    {
+        public WorkoutDay()
+        {
+
+        }
+
+        public int WorkoutDayId { get; set; }
+        public string WorkoutDayMonthWeek { get; set; }
+        public string WorkoutDayWeekDay { get; set; }
+
+        public virtual Workout Workout { get; set; }
+    }
+}
---
+++ NRD-Sporto-klubas/DB_API/sport-workouts-web-api/sport-workouts-web-api/Controllers/ExerciseController.cs
@@ -0,0 +1,68 @@
+﻿using sport_workouts_web_api.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace sport_workouts_web_api.Controllers
+{
+    public class ExerciseController : ApiController
+    {
+
+        // Post testavimo metodas. Priima per body paduodamus exercise parametrus
+        public IHttpActionResult Post(Exercise st)
+        {
+            using (var ctx = new WorkoutContext())
+            {
+                Exercise pp = new Exercise();
+
+                Workout ww = new Workout { WorkoutName = "Koju workoutas" };
+                MuscleGroup gg = new MuscleGroup {MuscleGroupName = "Kojos"};
+
+
+                try
+                {
+
+                    pp.ExerciseName = st.ExerciseName;
+                    pp.ExerciseImageFirst = st.ExerciseImageFirst;
+                    pp.ExerciseImageSecond = st.ExerciseImageSecond;
+                    pp.MuscleGroups.Add(gg);
+                    pp.Workouts.Add(ww);
+                    ctx.Exercises.Add(pp);
+                    ctx.SaveChanges();
+                }
+                catch (Exception Ex)
+                {
+                    var msg = Ex.Message;
+                    return NotFound();
+                }
+                return Ok("Okay");
+            }
+        }
+
+        /*
+        public List<Exercise> Get()
+        {
+            using (var ctx = new WorkoutContext())
+            {
+                List<Exercise> exercises = new List<Exercise>();
+                exercises = ctx.Exercises.ToList();
+                return exercises;
+            }
+        }
+
+        */
+
+        // Neveikia, nezinau kaip gettint turimus duomenis
+        public Exercise Get()
+        {
+            using (var ctx = new WorkoutContext())
+            {          
+                Exercise exer = ctx.Exercises.SingleOrDefault(ss => ss.ExerciseId == 1);
+                return exer;
+            }
+        }
+    }
+}
